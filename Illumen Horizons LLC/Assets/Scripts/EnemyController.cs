428d385cf0084c54d83a72a051cc7f5a2f891fb0
Gavin Schmidt <gavinschmidt47@yahoo.com>
2025-02-28 12:10:09 -0500

    Enemy Behavior done

--- Illumen Horizons LLC/Assets/Scripts/EnemyController.cs
+++ Illumen Horizons LLC/Assets/Scripts/EnemyController.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections.Generic;
+using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
@@ -15,13 +17,14 @@ public class EnemyController : MonoBehaviour
     public LayerMask playerLayer;
     private Vector3 directionToPlayer;
     private float angle;
+    private RaycastHit hit;
 
     //Enemy
     private Rigidbody rb;
     void Awake()
     {
         //change back to 2
-        gameInfo.ability = UnityEngine.Random.Range(0 , 1);
+        gameInfo.ability = UnityEngine.Random.Range(0 , 2);
 
         rb = GetComponent<Rigidbody>();
     }
@@ -47,11 +50,19 @@ public class EnemyController : MonoBehaviour
                     directionToPlayer = player.transform.position - transform.position;
                     angle = Vector3.Angle(directionToPlayer, -player.transform.forward);
 
-                    //If camera looking toward enemy and within distance
-                    if (angle < 85f && directionToPlayer.magnitude < 30f)
+                    //Raycast to check if player is in line of sight and within distance
+                    if (Physics.Raycast(transform.position, directionToPlayer, out hit, directionToPlayer.magnitude))
                     {
-                        agent.isStopped = true;
-                        rb.constraints = RigidbodyConstraints.FreezeAll;
+                        if (hit.collider.gameObject == player && angle < 85f && directionToPlayer.magnitude < 30f)
+                        {
+                            agent.isStopped = true;
+                            rb.constraints = RigidbodyConstraints.FreezeAll;
+                        }
+                        else
+                        {
+                            agent.isStopped = false;
+                            rb.constraints = RigidbodyConstraints.None;
+                        }
                     }
                     else
                     {
@@ -65,38 +76,62 @@ public class EnemyController : MonoBehaviour
                     directionToPlayer = player.transform.position - transform.position;
                     angle = Vector3.Angle(directionToPlayer, -player.transform.forward);
 
-                    //If camera looking toward enemy
-                    if (angle < 85f)
+                    //Raycast to check if player is in line of sight and within distance
+                    if (Physics.Raycast(transform.position, directionToPlayer, out hit, directionToPlayer.magnitude))
                     {
-                        agent.isStopped = false;
-                        rb.constraints = RigidbodyConstraints.FreezeAll;
+                        if (hit.collider.gameObject == player && angle < 85f)
+                        {
+                            agent.SetDestination(player.transform.position);
+                        }
+                        else
+                        {
+                            agent.SetDestination(transform.position);
+                        }
                     }
                     else
                     {
-                        agent.isStopped = true;
-                        rb.constraints = RigidbodyConstraints.None;
+                        agent.SetDestination(transform.position);
                     }
-                    agent.SetDestination(player.transform.position);
+                    
                     return;
                 case 2:
-                    Myers();
+                    //Vector to player and its angle to camera
+                    directionToPlayer = player.transform.position - transform.position;
+                    angle = Vector3.Angle(directionToPlayer, -player.transform.forward);
+
+                    //Raycast to check if player is in line of sight
+                    if (Physics.Raycast(transform.position, directionToPlayer, out hit, directionToPlayer.magnitude))
+                    {
+                        if (hit.collider.gameObject == player)
+                        {
+                            Debug.Log("Player in line of sight");
+                            agent.isStopped = true;
+                            rb.constraints = RigidbodyConstraints.FreezeAll;
+                        }
+                        else
+                        {
+                            Debug.Log("Player not in line of sight");
+                            agent.isStopped = false;
+                            rb.constraints = RigidbodyConstraints.None;
+                            agent.SetDestination(player.transform.position);
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("Raycast did not hit anything");
+                        agent.isStopped = false;
+                        rb.constraints = RigidbodyConstraints.None;
+                        agent.SetDestination(player.transform.position);
+                    }
                     return;
             }
         }
     }
 
-    void WeepingAngel()
+    private IEnumerator StopAgentForSeconds(float seconds)
     {
-        
-    }
-
-    void Enderman()
-    {
-
-    }
-
-    void Myers()
-    {
-
+        agent.isStopped = true;
+        yield return new WaitForSeconds(seconds);
+        agent.isStopped = false;
     }
 }
